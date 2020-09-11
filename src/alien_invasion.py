01804c8391564f1621777381dbf1240b5ae8e4e8
Nicole <adionicole@gmail.com>
2020-09-11 22:25:00 +0100

    Added ship to game screen

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -3,6 +3,10 @@ import sys
 import pygame
 
 from settings import Settings
+
+from ship import Ship
+
+
 class AlienInvasion:
     """An overall class to manage the games components and behaviour"""
     def __init__(self):
@@ -16,8 +20,8 @@ class AlienInvasion:
         
         pygame.display.set_caption("Alien Invasion")
         
-        # Sets colour of background
         
+        self.ship = Ship(self)
     
     def run_game(self):
         """Starts the main loop of the game """
@@ -25,8 +29,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Redraw the screen everytime the loop is passed
             self.screen.fill(self.settings.bg_colour)
-
+            self.ship.blitme()
+            
             pygame.display.flip()
 
 if __name__ == '__main__':
--- src/ship.py
+++ src/ship.py
@@ -5,15 +5,15 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
-        self.rect = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship from the images file and get its rect
         self.image = pygame.image.load("src/Images/Spaceship Icon.png")
         self.rect = self.image.get_rect()
 
         # Start every new ship at the bottom centre of the screen
-        self.scree.blit(self.image.rect)
+        self.rect.midbottom = self.screen_rect.midbottom
  
     def blitme(self):
         """ This draws the ship """
-        self.screen.blit(self.images, self.rect)
+        self.screen.blit(self.image, self.rect)
